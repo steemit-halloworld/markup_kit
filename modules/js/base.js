11d49fd38609939316c81529d4699513ff3a3be1
Thomas Schroeter <public@tschroeter.com>
2017-06-26 13:04:18 +0200

    - global kit namespace and core object refactored - kit.dom module refactored

--- modules/js/base.js
+++ modules/js/base.js
@@ -1,6 +1,126 @@
+// Kit core
+// http://getmarkupkit.com
+// (c) Thomas Schroeter <mailto: mail@tschroeter.com> and Investigative Reporters & Editors 2017
+// markup_kit may be freely distributed under the MIT license
+
+/**
+ *
+ * Kit is a JavaScript framework for building client- or server-side applications.
+ *
+ * global.kit is the global namespace for all kit components and the core module
+ * where components are registered and managed.
+ *
+ */
+const kit = (function ()
+{
+  "use strict";
+
+  const kit = Object.create(Object.prototype, {
+    global: {writable: false, value: Function('return this')()}
+  });
+
+
+  const COMPONENT_SELECTOR = '[kit-component]';
+
+//!@{
+//<!------------------------------------------------------------------------------------------------------------------->
+//! @name Managing components
+//<!------------------------------------------------------------------------------------------------------------------->
+//region
+
+  const components = {}, instances = {};
+
+  /**
+   * Registers a new component
+   * @param {String} name
+   * @param {Function} creator_fn Factory function used to create the component
+   * @throws {Error} If a component has already been registered
+   */
+  kit.register = function (name, creator_fn)
+  {
+    components[name] = {
+      creator_fn: creator_fn,
+      ref_counter: 1
+    };
+
+    return this;
+  };
+
+  function component_name (html_el)
+  {
+    const attr = html_el.getAttribute('kit-component');
+    if (attr) return attr.split(' ')[0];
+
+    return '';
+  }
+
+  function purge_instances ()
+  {
+    for (var i = 0, len = instances.length; i < len; i++)
+    {
+      if (!kit.dom.is_detached(instances[i].html_el)) continue;
+
+      delete instances[i];
+    }
+  }
+
+//endregion
+
+//!@{
+//<!------------------------------------------------------------------------------------------------------------------->
+//! @name Mounting && Un-mounting components
+//<!------------------------------------------------------------------------------------------------------------------->
+//region
+
+  /**
+   * Mounts all components contained within an element
+   * @param {HTMLElement} html_el DOM element which contains components
+   */
+  function mount_all (html_el)
+  {
+    const module_elements = kit.dom.query_all(html_el, COMPONENT_SELECTOR);
+    for (var i = 0, len = module_elements.length; i < len; i++)
+    {
+      mount(module_elements[i]);
+    }
+
+    return kit;
+  }
+
+  function mount (html_el)
+  {
+    const component_name = component_name(html_el),
+    component_data = components[component_name];
+
+    var instance_data = instances[html_el];
+    if (typeof instance_data == 'object') return instance_data.instance;
+
+    component_data.ref_counter++;
+
+    const component = component_data.creator_fn();
+    instance_data = {
+      name: component_data,
+      instance: component,
+      html_el: html_el
+    };
+
+    instance_data[html_el] = instance_data;
+
+    purge_instances();
+
+    return component;
+  }
+
+//endregion
+  return (kit.global.kit = kit.global.kit || kit);
+
+})();
+
+
 (function ()
 {
   "use strict";
+
 
   var root = global_scope(self);
 
@@ -8,6 +128,11 @@
   root.global_scope = function (self)
   {
     return global_scope(self);
+  };
+
+  function global_scope (self)
+  {
+    return kit.global;
   };
 
   root.string = function string (str, variables)
@@ -20,7 +145,7 @@
 
   root.elements_by_xpath = function (xpath, dom)
   {
-    if (!is_defined (dom)) dom = root.document;
+    if (!root.is_defined(dom)) dom = root.document;
 
     var result = [];
     var nodesSnapshot = dom.evaluate(xpath, dom, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
@@ -60,14 +185,6 @@
   root.is_defined = function (val)
   {
     return val != null;
-  }
-
-  function global_scope (self)
-  {
-    var is_browser_env = typeof self == 'object' && self.self === self;
-    var is_server_env = !is_browser_env && typeof global == 'object' && global.global === global;
-    var scope = is_browser_env && self || is_server_env && global || {};
-    return scope;
   }
 
 
--- modules/js/dom.js
+++ modules/js/dom.js
@@ -1,3 +1,72 @@
+// Kit DOM
+// http://getmarkupkit.com
+// (c) Thomas Schroeter <mailto: mail@tschroeter.com> and Investigative Reporters & Editors 2017
+// markup_kit may be freely distributed under the MIT license
+
+/**
+ * DOM abstraction to use native browser functionality
+ * @type {Function}
+ */
+kit.native_dom = (function ()
+{
+  "use strict";
+
+  const obj = {
+    /**
+     * Returns the first element that is descendant from the given root element
+     * @param {HTMLElement} root_el
+     * @param selector
+     * @returns {Element}
+     */
+    query: function (root_el, selector)
+    {
+      return root_el.querySelector(selector);
+    },
+
+    /**
+     * Returns a non-live NodeList of all elements descended from the given root element
+     * @param {HTMLElement} root_el
+     * @param selector
+     * @returns {NodeList}
+     */
+
+    query_all: function (root_el, selector)
+    {
+      return root_el.querySelectorAll(selector);
+    },
+
+    on: function (element, type, listener)
+    {
+      element.addEventListener(type, listener, false);
+    },
+
+    off: function (element, type, listener)
+    {
+      element.removeEventListener(type, listener, false);
+    },
+
+    is_detached: function (html_el)
+    {
+      if (html_el === kit.dom.root_el) return false;
+
+      while (html_el && html_el.parentNode)
+      {
+        if (html_el === document.documentElement) return false;
+        html_el = html_el.parentNode;
+      }
+
+      return true;
+    }
+  };
+
+  Object.defineProperty(obj, 'root_el', {writable:false, value: kit.global.document.documentElement});
+
+  return obj;
+});
+
+kit.dom = kit.native_dom;
+
+
 (function ()
 {
   var root = global_scope(self);
@@ -18,11 +87,11 @@
 
   root.is_detached = function (el)
   {
-    if(el === document.documentElement) return false;
+    if (el === document.documentElement) return false;
 
     while (el && el.parentNode)
     {
-      if(el === document.documentElement) return false;
+      if (el === document.documentElement) return false;
       el = el.parentNode;
     }
 
