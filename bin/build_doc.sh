3b46ff96e41f457c53641a10b00b17b58dac6e17
Valentindi <vadittmar@googlemail.com>
2017-08-01 16:26:48 +0200

    Comparison: Fixed postcss build process

--- bin/build_doc.sh
+++ bin/build_doc.sh
@@ -3,7 +3,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 mkdir -p "$parent_path"/../doc/md
 mkdir -p "$parent_path"/../doc/html
 
-CSS_FILES=("$parent_path"/../modules/css/*.css)
+CSS_FILES=("$parent_path"/../modules/markup_kit/css/*.css)
 
 css_markdown_args=()
 for f in ${CSS_FILES}; do
@@ -11,4 +11,4 @@ for f in ${CSS_FILES}; do
 done
 
 css-markdown "${CSS_FILES[@]}" > "$parent_path"/../doc/md/documentation.md
-pandoc --template "$parent_path"/../modules/html/pandoc_template.html  --toc --toc-depth 2 --css ../../build/css/style.stage1.css -f markdown_strict -t html --standalone -o "$parent_path"/../doc/html/documentation.html "$parent_path"/../doc/md/documentation.md
+pandoc --template "$parent_path"/../modules/markup_kit/html/pandoc_template.html  --toc --toc-depth 2 --css ../../build/css/style.stage1.css -f markdown_strict -t html --standalone -o "$parent_path"/../doc/html/documentation.html "$parent_path"/../doc/md/documentation.md
