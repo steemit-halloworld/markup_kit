63533f1f111f1d063c21d9fc4ef8eefd410865d0
Thomas Schroeter <public@tschroeter.com>
2018-07-10 11:21:37 +0200

    encapsulate DOM patching into an object

--- modules/markup_kit/js/kit.dom.patch.js
+++ modules/markup_kit/js/kit.dom.patch.js
@@ -1,26 +1,46 @@
 (function () {
   "use strict";
 
-  var last_patch_time = 0;
+//!@{
+//<!------------------------------------------------------------------------------------------------------------------->
+//! @name Construction & Initialisation
+//<!------------------------------------------------------------------------------------------------------------------->
+//region
 
+  var ref_counter = -1;
+  var prv = [];
+  var last_patch_time = 0;
   var scope = new kit.global.Scope();
 
-  kit.dom.patch_html = function (html_text, ignore_fn) {
-    var doc = document.implementation.createHTMLDocument("example");
-    doc.documentElement.innerHTML = html_text;
+  kit.dom.Patch = function () {
+    var id = (++ref_counter);
+    prv[id] = {};
 
-    return kit.dom.patch(document.body, doc.body.firstElementChild, document.body.firstElementChild, 0, ignore_fn);
+    prv[id].callbacks = [];
+    prv[id].ignore_callbacks = [];
+
+    this.id = function () {return id;};
   };
 
-  kit.dom.patch = function (parent_el, new_el, old_el, old_el_position, ignore_fn) {
-    var is_patched = patch_element(parent_el, new_el, old_el, old_el_position, ignore_fn);
-    if (is_patched)
-    {
-      last_patch_time = new Date().getMilliseconds();
-      scope.digest();
-    }
+  var proto = kit.dom.Patch.prototype;
 
-    return is_patched;
+//endregion
+
+//!@{
+//<!------------------------------------------------------------------------------------------------------------------->
+//! @name Callbacks
+//<!------------------------------------------------------------------------------------------------------------------->
+//region
+
+  proto.callback = function (callback) {
+    prv[this.id()].callbacks.push(callback);
+  };
+
+  proto.ignore = function (callback) {
+    prv[this.id()].ignore_callbacks.push(callback);
+  };
+
+//endregion
 
 //!@{
 //<!------------------------------------------------------------------------------------------------------------------->
@@ -28,110 +48,109 @@
 //<!------------------------------------------------------------------------------------------------------------------->
 //region
 
-    function remove_boolean_attribute (target, name)
+  function remove_boolean_attribute (target, name)
+  {
+    //console.log("REMOVE BOOLEAN ATTRIBUTE: " + text(target) + " - " +  text(name));
+    target.removeAttribute(name);
+    target[name] = false;
+  }
+
+  function set_string_attribute (target, name, value)
+  {
+    var old_val = target.getAttribute(name);
+    if (old_val != value) target.setAttribute(name, value);
+
+    old_val = target[name];
+    if (!kit.is_defined(old_val) || old_val == value) return;
+
+    target[name] = value;
+  }
+
+  function set_boolean_attribute (target, name, value)
+  {
+    value = !value ? '' : value;
+    target.setAttribute(name, value);
+    target[name] = (['true', ''].indexOf(value) > -1);
+  }
+
+  function remove_attribute (target, name, value)
+  {
+    if (typeof value === 'boolean')
     {
-      //console.log("REMOVE BOOLEAN ATTRIBUTE: " + text(target) + " - " +  text(name));
+      remove_boolean_attribute(target, name);
+    }
+    else
+    {
       target.removeAttribute(name);
-      target[name] = false;
     }
+  }
 
-    function set_string_attribute (target, name, value)
+  function set_attribute (target, name, value)
+  {
+    if (typeof value === 'boolean' || ['selected', 'checked', 'disabled', 'hidden'].indexOf(name) >= 0)
     {
-      var old_val = target.getAttribute(name);
-      if(old_val != value) target.setAttribute(name, value);
-
-      old_val = target[name];
-      if(!kit.is_defined(old_val) || old_val == value) return;
-
-      target[name] = value;
+      set_boolean_attribute(target, name, value);
     }
-
-    function set_boolean_attribute (target, name, value)
+    else
     {
-      value = !value ? '' : value;
-      target.setAttribute(name, value);
-      target[name] = (['true', ''].indexOf(value)>-1);
+      set_string_attribute(target, name, value);
     }
+  }
 
-    function remove_attribute (target, name, value)
+  function update_attribute (target, name, new_val, old_val)
+  {
+    if (new_val == null)
+    { // undefined or null
+      remove_attribute(target, name, old_val);
+      return true;
+    }
+    else if (old_val == null || new_val !== old_val)
     {
-      if (typeof value === 'boolean')
-      {
-        remove_boolean_attribute(target, name);
-      }
-      else
-      {
-        target.removeAttribute(name);
-      }
+      set_attribute(target, name, new_val);
+      return true;
     }
 
-    function set_attribute (target, name, value)
+    return false;
+  }
+
+  function to_set (named_node_map, set)
+  {
+    for (var i = 0; i < named_node_map.length; ++i)
     {
-      if (typeof value === 'boolean' || ['selected', 'checked', 'disabled', 'hidden'].indexOf(name)>=0)
-      {
-        set_boolean_attribute(target, name, value);
-      }
-      else
-      {
-        set_string_attribute(target, name, value);
-      }
+      set.add(named_node_map[i].nodeName);
     }
 
-    function update_attribute (target, name, new_val, old_val)
-    {
-      if (new_val == null)
-      { // undefined or null
-        remove_attribute(target, name, old_val);
-        return true;
-      }
-      else if (old_val == null || new_val !== old_val)
-      {
-        set_attribute(target, name, new_val);
-        return true;
-      }
+    return set;
+  }
 
-      return false;
-    }
+  function attr_val (name, attr_node)
+  {
+    return attr_node ? attr_node.nodeValue : null;
+  }
 
-    function to_set (named_node_map, set)
-    {
-      for (var i = 0; i < named_node_map.length; ++i)
-      {
-        set.add(named_node_map[i].nodeName);
-      }
+  function update_attributes (target, new_node, old_node)
+  {
+    var new_attributes = new_node.attributes;
+    var old_attributes = old_node.attributes;
 
-      return set;
-    }
+    var key_set = to_set(new_attributes, new Set());
+    key_set = to_set(old_attributes, key_set);
 
-    function attr_val (name, attr_node)
-    {
-      return attr_node ? attr_node.nodeValue : null;
-    }
+    var result = false;
+    key_set.forEach(function (name) {
+      var old_attr = old_attributes.getNamedItem(name);
+      var new_attr = new_attributes.getNamedItem(name);
 
-    function update_attributes (target, new_node, old_node)
-    {
-      var new_attributes = new_node.attributes;
-      var old_attributes = old_node.attributes;
+      var old_val = attr_val(name, old_attr);
+      var new_val = attr_val(name, new_attr);
 
-      var key_set = to_set(new_attributes, new Set());
-      key_set = to_set(old_attributes, key_set);
+      var x = update_attribute(target, name, new_val, old_val);
 
-      var result = false;
-      key_set.forEach(function(name)
-      {
-        var old_attr = old_attributes.getNamedItem(name);
-        var new_attr = new_attributes.getNamedItem(name);
+      result = result || x;
+    });
 
-        var old_val = attr_val(name, old_attr);
-        var new_val = attr_val(name, new_attr);
-
-        var x = update_attribute(target, name, new_val, old_val);
-
-        result = result || x;
-      });
-
-      return result;
-    }
+    return result;
+  }
 
 //endregion
 //!@{
@@ -140,97 +159,144 @@
 //<!------------------------------------------------------------------------------------------------------------------->
 //region
 
-    function has_node_changed (node1, node2)
+  function has_node_changed (node1, node2)
+  {
+    return node1.nodeName !== node2.nodeName || text(node1) !== text(node2);
+  }
+
+  function text (node)
+  {
+    if (typeof node === 'string') return node;
+
+    var child_count = node.childNodes === undefined ? 0 : node.childNodes.length;
+    var text = "";
+    for (var i = 0; i < child_count; i++)
     {
-      return node1.nodeName !== node2.nodeName || text(node1) !== text(node2);
+      var n = node.childNodes[i];
+      if (n.nodeType != Node.TEXT_NODE) continue;
+
+      text += n.nodeValue;
     }
 
-    function text (node)
+    return text.trim();
+  }
+
+  proto.patch = function (parent_el, new_el, old_el, old_el_position, ignore_fn) {
+    var is_patched = patch_element(this, parent_el, new_el, old_el, old_el_position);
+    if (is_patched)
     {
-      if (typeof node === 'string') return node;
-
-      var child_count = node.childNodes === undefined ? 0 : node.childNodes.length;
-      var text = "";
-      for (var i = 0; i < child_count; i++)
-      {
-        var n = node.childNodes[i];
-        if (n.nodeType != Node.TEXT_NODE) continue;
-
-        text += n.nodeValue;
-      }
-
-      return text.trim();
+      last_patch_time = new Date().getMilliseconds();
+      scope.digest();
     }
 
-    function patch_element (parent_el, new_el, old_el, position, ignore_fn)
+    prv[this.id()].callbacks.push(ignore_fn);
+
+    return is_patched;
+  };
+
+  function patch_element (owner, parent_el, new_el, old_el, position)
+  {
+    var dirty_nodes = [];
+
+    position = position || 0;
+
+
+    var result = update_element(owner, parent_el, new_el, old_el, position, dirty_nodes);
+
+    for (var i in dirty_nodes)
     {
-      var dirty_nodes = [];
+      dirty_nodes[i].parentNode.removeChild(dirty_nodes[i]);
+    }
 
-      position = position || 0;
+    return result;
+  }
 
+  function ignore (owner, el)
+  {
+    var len = prv[owner.id()].ignore_callbacks.length;
+    for (var i = 0; i < len; i++) if (prv[owner.id()].ignore_callbacks[i](el)) return true;
+    return false;
+  }
 
-      var result = update_element(parent_el, new_el, old_el, position, dirty_nodes, ignore_fn);
+  function on_update_element (owner, parent, new_node, old_node, index)
+  {
+    var len = prv[owner.id()].callbacks.length;
+    for (var i = 0; i < len; i++)
+    {
+      if (!prv[owner.id()].callbacks[i](owner, parent, old_node, new_node, index)) return false;
+    }
+    return true;
+  }
 
-      for (var i in dirty_nodes)
+  function update_element (owner, parent, new_node, old_node, index, dirty_nodes)
+  {
+    if (!old_node)
+    {
+      parent.appendChild(new_node.cloneNode(true));
+      return true;
+    }
+    else if (!new_node)
+    {
+      dirty_nodes.push(parent.children[index]);
+      return true;
+    }
+    else if (has_node_changed(new_node, old_node))
+    {
+      parent.replaceChild(new_node.cloneNode(true), parent.children[index]);
+      return true;
+    }
+    else if (new_node)
+    {
+      var result = update_attributes(parent.children[index], new_node, old_node);
+
+      var new_child_count = new_node.children.length;
+      var old_child_count = old_node.children.length;
+
+      for (var i = 0, j = 0; i < new_child_count || j < old_child_count; i++, j++)
       {
-        dirty_nodes[i].parentNode.removeChild(dirty_nodes[i]);
+        var n_new_node = new_node.children[i];
+
+        var n_old_node;
+
+        j--;
+        do
+        {
+          j++;
+          n_old_node = old_node.children[j];
+
+        } while (ignore(owner, n_old_node, n_new_node));
+
+        if (n_new_node === undefined && n_old_node === undefined) continue;
+
+        if (on_update_element(owner, parent.children[index], n_new_node, n_old_node, j))
+        {
+          var x = update_element(owner, parent.children[index], n_new_node, n_old_node, j, dirty_nodes);
+          result = result || x;
+
+        }
       }
 
       return result;
     }
 
-    function update_element (parent, new_node, old_node, index, dirty_nodes, ignore_fn)
-    {
-      if (!old_node)
-      {
-        parent.appendChild(new_node.cloneNode(true));
-        return true;
-      }
-      else if (!new_node)
-      {
-        dirty_nodes.push(parent.children[index]);
-        return true;
-      }
-      else if (has_node_changed(new_node, old_node))
-      {
-        parent.replaceChild(new_node.cloneNode(true), parent.children[index]);
-        return true;
-      }
-      else if (new_node)
-      {
-        var result = update_attributes(parent.children[index], new_node, old_node);
-
-        var new_child_count = new_node.children.length;
-        var old_child_count = old_node.children.length;
-
-        for (var i = 0, j = 0; i < new_child_count || j < old_child_count; i++, j++)
-        {
-          var n_new_node = new_node.children[i];
-
-          var n_old_node;
-
-          j--;
-          do
-          {
-            j++;
-            n_old_node = old_node.children[j];
-
-          } while (ignore_fn && ignore_fn(n_old_node));
-
-          if (n_new_node === undefined && n_old_node === undefined) continue;
-
-          var x = update_element(parent.children[index], n_new_node, n_old_node, j, dirty_nodes, ignore_fn);
-          result = result || x;
-        }
-
-        return result;
-      }
-
-      return false;
-    }
+    return false;
+  }
 
 //endregion
+
+  /* deprecated using new kit.dom.Patch().patch(...) instead */
+  kit.dom.patch_html = function (html_text, ignore_fn) {
+    var doc = document.implementation.createHTMLDocument("example");
+    doc.documentElement.innerHTML = html_text;
+
+    return kit.dom.patch(document.body, doc.body.firstElementChild, document.body.firstElementChild, 0, ignore_fn);
   };
+
+  kit.dom.patch = function (parent_el, new_el, old_el, old_el_position, ignore_fn) {
+    var patcher = new kit.dom.Patch();
+    return patcher.patch(parent_el, new_el, old_el, old_el_position, ignore_fn);
+  };
+
 
   kit.dom.patch.on_patch = function (callback_fn) {
     scope.watch(kit.dom.patch.last_patch_time, callback_fn);
@@ -239,6 +305,7 @@
   kit.dom.patch.last_patch_time = function () {
     return last_patch_time;
   }
+
 
 }());
 
